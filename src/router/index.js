d0087f35d0c2651bb36a8ca0a34838d1ff09dfaa
EsterPozo <pozo.estherr@gmail.com>
2022-12-12 12:40:28 +0100

    add pinia persist + netlify

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from '../views/Dashboard.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
    {
     path: "/", 
     component: Dashboard,
-   }
+   },
   ]
 })
 
