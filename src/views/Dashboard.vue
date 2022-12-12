1b80e61faa13c4f7a3be73a49d783085093b93f6
EsterPozo <pozo.estherr@gmail.com>
2022-12-12 13:20:12 +0100

    include mapStores pinia

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,20 @@
 <template>
-    Dashboard
+  Dashboard
+  <button @click="login"></button>
 </template>
 
 <script>
+import { mapStores } from "pinia";
+/* import { userStore } from "../stores/user.js"; */
+
 export default {
-
-}
-
+  computed: {
+    ...mapStores(""),
+  },
+  methods: {
+    login() {
+      this.userStore.singUp();
+    },
+  },
+};
 </script>
