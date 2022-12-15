f5cc1ad3f98d970fe7144204f04223822f09baf7
EsterPozo <pozo.estherr@gmail.com>
2022-12-15 17:28:58 +0100

    header menu responsive - pending animation on burger

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,20 @@
 <template>
   <div
-    class="bg-zinc-700 text-zinc-300 flex flex-row justify-between items-center"
+    class="bg-zinc-700 text-zinc-300 flex flex-row justify-between items-center pt-[5px] pb-[5px]"
   >
-    <div class="ml-[20px] flex flex-row justify-start items-center">
+    <div class="ml-[20px] flex flex-row  justify-between w-[100%] md:w-auto items-center">
+      <div>
       <img src="../assets/images/art.png" alt="logo" class="w-[50px]" />
-
-      <button @click="toggleDrop = !toggleDrop">
+    </div>
+    <div>
+      <button @click="toggleDrop = !toggleDrop; open =!open">
         <div class="space-y-2 block md:hidden ml-5">
           <span class="block w-8 h-0.5 bg-zinc-300"></span>
           <span class="block w-8 h-0.5 bg-zinc-300"></span>
           <span class="block w-5 h-0.5 bg-zinc-300"></span>
         </div>
       </button>
+    </div>
       
       <ul class="ml-[10px] md:flex md:flex-row hidden">
         <li><a href="#" class="mx-3 hover:text-violet-400">Home</a></li>
@@ -29,7 +32,7 @@
       </p>
       <button
         @click="logOut()"
-        class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-6 py-2.5 text-center my-3"
+        class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-6 py-2.5 text-center my-3 md:block hidden"
       >
         Log out!
       </button>
@@ -37,10 +40,12 @@
     </div>
   </div>
   <div v-if="toggleDrop" class="bg-zinc-600 text-zinc-300 pb-[5px] block md:hidden">
-  <ul>
-    <li><a href="#" class="mx-[90px] hover:text-violet-400">Home</a></li>
-        <li><a href="#" class="mx-[90px] hover:text-violet-400">Dashboards</a></li>
-        <li><a href="#" class="mx-[90px] hover:text-violet-400">Contact Us</a></li>
+  <ul class="flex flex-col items-end">
+    
+    <li><a href="#" class="mr-[15px] hover:text-violet-400">Home</a></li>
+        <li><a href="#" class="mr-[15px] text-end hover:text-violet-400">Dashboards</a></li>
+        <li><a href="#" class="mr-[15px] hover:text-violet-400">Contact Us</a></li>
+      
   </ul> </div>
 </template>
 
@@ -52,7 +57,8 @@ import userStore from "../stores/user.js";
 export default {
   data() {
     return {
-      toggleDrop: Boolean,
+      toggleDrop: false,
+      open: false,
     };
   },
   computed: {
