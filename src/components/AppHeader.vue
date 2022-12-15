feb4ad934179add887af8b3719cad106cf899b16
EsterPozo <pozo.estherr@gmail.com>
2022-12-15 17:01:31 +0100

    dropdown menu done - header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,46 +1,63 @@
 <template>
   <div
-    class="bg-zinc-600 text-zinc-300 flex flex-row justify-between items-center"
+    class="bg-zinc-700 text-zinc-300 flex flex-row justify-between items-center"
   >
     <div class="ml-[20px] flex flex-row justify-start items-center">
       <img src="../assets/images/art.png" alt="logo" class="w-[50px]" />
-      
-      
-      
 
-
-      <ul class="ml-[10px]">
-        <a href="#" class="mx-3">Home</a>
-        <a href="#" class="mx-3">Dashboards</a>
-        <a href="#" class="mx-3">Contact Us</a>
+      <button @click="toggleDrop = !toggleDrop">
+        <div class="space-y-2 block md:hidden ml-5">
+          <span class="block w-8 h-0.5 bg-zinc-300"></span>
+          <span class="block w-8 h-0.5 bg-zinc-300"></span>
+          <span class="block w-5 h-0.5 bg-zinc-300"></span>
+        </div>
+      </button>
+      
+      <ul class="ml-[10px] md:flex md:flex-row hidden">
+        <li><a href="#" class="mx-3 hover:text-violet-400">Home</a></li>
+        <li><a href="#" class="mx-3 hover:text-violet-400">Dashboards</a></li>
+        <li><a href="#" class="mx-3 hover:text-violet-400">Contact Us</a></li>
       </ul>
+      
     </div>
     <div class="mr-[20px] flex flex-row items-center">
-      <p class="mr-3">Welcome, {{userStore.user.email.substring(0, userStore.user.email.indexOf('@'))}} </p>
+      <p class="mr-3 text-zinc-100 text-xs md:block hidden">
+        Welcome,
+        {{
+          userStore.user.email.substring(0, userStore.user.email.indexOf("@"))
+        }}
+      </p>
       <button
         @click="logOut()"
         class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-6 py-2.5 text-center my-3"
       >
         Log out!
       </button>
-    </div> 
+      
+    </div>
   </div>
+  <div v-if="toggleDrop" class="bg-zinc-600 text-zinc-300 pb-[5px] block md:hidden">
+  <ul>
+    <li><a href="#" class="mx-[90px] hover:text-violet-400">Home</a></li>
+        <li><a href="#" class="mx-[90px] hover:text-violet-400">Dashboards</a></li>
+        <li><a href="#" class="mx-[90px] hover:text-violet-400">Contact Us</a></li>
+  </ul> </div>
 </template>
 
 <script>
 import { mapStores } from "pinia";
 import userStore from "../stores/user.js";
-import { ref } from 'vue';
+
 
 export default {
-  setup() {
-    let showMenu = ref(false);
-    const toggleNav = () => (showMenu.value = !showMenu.value);
-    return { showMenu, toggleNav };
+  data() {
+    return {
+      toggleDrop: Boolean,
+    };
   },
   computed: {
     ...mapStores(userStore),
-   
+     
   },
   data() {
     return {};
@@ -50,8 +67,9 @@ export default {
       await this.userStore.signOut();
     },
     username() {
-      return userStore.user.email
-    }
+      return userStore.user.email;
+    },
+    
   },
 };
 </script>
