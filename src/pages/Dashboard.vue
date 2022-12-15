c6c2344d91652d38642ac15638eb9c3adce6f961
Anastasiia <118ana@gmail.com>
2022-12-15 14:45:51 +0100

    update textarea auto, divide new tasks

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -32,13 +32,8 @@
         <h2>To do</h2>
         <div v-for="(task, index) in tasksStore.todoArr">
           <TaskItem :task="task" />
-          <br />
-          <br />
         </div>
-
-        <button type="submit" @click="viewNews(toggleToDo)">+ Add New Task</button>
-        <br />
-        <br />
+        <button type="submit" @click="viewToDo()">+ Add New Task</button>
         <div v-if="toggleToDo">
           <textarea
             v-model="title"
@@ -48,23 +43,17 @@
             rows="3"
             placeholder="Enter the text of your taks"
           ></textarea>
-          <br />
-          <br />
-          <button type="submit" @click="createTask(title)">Add</button>
+          <button type="submit" @click="createTask(title, 0);viewToDo();deleteText()">Add</button>
+          <button type="reset" @click="deleteText()">reset</button>
         </div>
       </div>
       <div class="columna">
         <h2>Ongoing</h2>
         <div v-for="(task, index) in tasksStore.ongoingArr">
           <TaskItem :task="task" />
-          <br />
-          <br />
         </div>
-
-        <button type="submit" @click="viewNews(toggleDoing)">+ Add New Task</button>
-        <br />
-        <br />
-        <div v-if="toggleDoing === true">
+        <button type="submit" @click="viewDoing">+ Add New Task</button>
+        <div v-if="toggleDoing">
           <textarea
             v-model="title"
             name="textarea"
@@ -73,23 +62,17 @@
             rows="3"
             placeholder="Enter the text of your taks"
           ></textarea>
-          <br />
-          <br />
-          <button type="submit" @click="createTask(title)">Add</button>
+          <button type="submit" @click="createTask(title, 1);viewDoing();deleteText()">Add</button>
+          <button type="reset" @click="deleteText()">reset</button>
         </div>
       </div>
       <div class="columna">
         <h2>Done</h2>
         <div v-for="(task, index) in tasksStore.doneArr">
           <TaskItem :task="task" />
-          <br />
-          <br />
         </div>
-
-        <button type="submit" @click="viewNews(toggleDone)">+ Add New Task</button>
-        <br />
-        <br />
-        <div v-if="toggleDone === true">
+        <button type="submit" @click="viewDone">+ Add New Task</button>
+        <div v-if="toggleDone">
           <textarea
             v-model="title"
             name="textarea"
@@ -98,9 +81,8 @@
             rows="3"
             placeholder="Enter the text of your taks"
           ></textarea>
-          <br />
-          <br />
-          <button type="submit" @click="createTask(title)">Add</button>
+          <button type="submit" @click="createTask(title, 2);viewDone();deleteText()">Add</button>
+          <button type="reset" @click="deleteText()">reset</button>
         </div>
       </div>
     </div>
@@ -139,30 +121,32 @@ export default {
       const response = await this.tasksStore.fetchTasks();
       this.tasks = this.tasksStore.tasks;
     }, */
-    async createTask(title) {
+    async createTask(title, status) {
       const response2 = await this.tasksStore.createTask(
         this.userStore.user.id,
         title,
-        0
+        status
       );
       const response3 = await this.tasksStore.fetchTasks();
     },
-    viewNews(toggle) {
-      /* if (this.viewNew === false) {
-        this.viewNew = true;
-      } else if (this.viewNew === true) {
-        this.viewNew = false;
-      } */
-        toggle === !toggle;
-        console.log("toggle", toggle)
+    viewToDo() {
+        this.toggleToDo = !this.toggleToDo;
     },
+    viewDoing() {
+        this.toggleDoing = !this.toggleDoing;
+    },
+    viewDone() {
+        this.toggleDone = !this.toggleDone;
+    },
+    deleteText(){
+      this.title = '';
+    }
   },
   components: {
     TaskItem,
     AppHeader,
   },
   mounted() {
-    console.log("mounted!");
     this.tasksStore.fetchTasks();
    //hacer el loading
   },
