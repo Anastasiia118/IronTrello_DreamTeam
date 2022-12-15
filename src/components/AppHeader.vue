238897374abf7ea526dda703b360ebe910da1892
EsterPozo <pozo.estherr@gmail.com>
2022-12-15 15:25:32 +0100

    heaader button changed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,7 @@
     
   </div>
   <div class="mr-[20px]">
-    <button @click="logOut()" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center my-3">Log out!</button>
+    <button @click="logOut()" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-6 py-2.5 text-center my-3">Log out!</button>
   </div>
 </div>
 </template>
