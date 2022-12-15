b7b8bc259b72eeaca81f01d35e1faa73e4c591f4
EsterPozo <pozo.estherr@gmail.com>
2022-12-15 16:17:30 +0100

    welcome on header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,31 +1,56 @@
 <template>
-  <div class=" bg-zinc-600 text-zinc-300 flex flex-row justify-between items-center">
-  <div class="ml-[20px] flex flex-row justify-start items-center">
-    <img src="../assets/images/art.png" alt="logo" class="w-[50px]">
-    <ul class="ml-[10px]">
-      <a href="#" class="mx-3">Home</a>
-      <a href="#" class="mx-3">Dashboards</a>
-      <a href="#" class="mx-3">Contact Us</a>
-    </ul>
-    
+  <div
+    class="bg-zinc-600 text-zinc-300 flex flex-row justify-between items-center"
+  >
+    <div class="ml-[20px] flex flex-row justify-start items-center">
+      <img src="../assets/images/art.png" alt="logo" class="w-[50px]" />
+      
+      
+      
+
+
+      <ul class="ml-[10px]">
+        <a href="#" class="mx-3">Home</a>
+        <a href="#" class="mx-3">Dashboards</a>
+        <a href="#" class="mx-3">Contact Us</a>
+      </ul>
+    </div>
+    <div class="mr-[20px] flex flex-row items-center">
+      <p class="mr-3">Welcome, {{userStore.user.email.substring(0, userStore.user.email.indexOf('@'))}} </p>
+      <button
+        @click="logOut()"
+        class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-6 py-2.5 text-center my-3"
+      >
+        Log out!
+      </button>
+    </div> 
   </div>
-  <div class="mr-[20px]">
-    <button @click="logOut()" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-6 py-2.5 text-center my-3">Log out!</button>
-  </div>
-</div>
 </template>
 
 <script>
 import { mapStores } from "pinia";
 import userStore from "../stores/user.js";
+import { ref } from 'vue';
 
 export default {
+  setup() {
+    let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);
+    return { showMenu, toggleNav };
+  },
   computed: {
     ...mapStores(userStore),
+   
+  },
+  data() {
+    return {};
   },
   methods: {
-   async logOut(){
-        await this.userStore.signOut();
+    async logOut() {
+      await this.userStore.signOut();
+    },
+    username() {
+      return userStore.user.email
     }
   },
 };
